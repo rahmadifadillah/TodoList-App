783b92bb57a0bfdc6eb52b33a44e6587a6f67aed
Rahmadi Fadillah <rahmadifadillah83@gmail.com>
2023-05-25 14:40:03 +0700

    add todoitem jsx

--- src/component/TodoItem.jsx
+++ src/component/TodoItem.jsx
@@ -1,7 +1,61 @@
-import React, { userRef } from "react";
+import React, { useRef } from "react";
+import {
+  IoCreate,
+  IoCheckmarkCircleSharp,
+  IoCloseCircle,
+} from "react-icons/io5";
 
-const TodoItem = () => {
-  return <div>TodoItem</div>;
+const TodoItem = (props) => {
+  const { item, updateTodo, removeTodo, completeTodo } = props;
+
+  const inputRef = useRef(true);
+
+  const changeFocus = () => {
+    inputRef.current.disabled = false;
+    inputRef.current.focus();
+  };
+
+  const update = (id, value, e) => {
+    if (e.which === 13) {
+      //here 13 is key code for enter key
+      updateTodo({
+        id,
+        item: value,
+      });
+      inputRef.current.disabled = true;
+    }
+  };
+  return (
+    <li key={item.id} className="card">
+      <textarea
+        ref={inputRef}
+        disabled={inputRef}
+        defaultValue={item.item}
+        onKeyPress={(e) => update(item.id, inputRef.current.value, e)}
+      />{" "}
+      <div className="btns">
+        <button onClick={() => changeFocus()}>
+          {" "}
+          <IoCreate />{" "}
+        </button>{" "}
+        {item.completed === false && (
+          <button
+            style={{
+              color: "green",
+            }}
+            onClick={() => completeTodo(item.id)}
+          >
+            <IoCheckmarkCircleSharp />
+          </button>
+        )}{" "}
+        <button style={{ color: "red" }} onClick={() => removeTodo(item.id)}>
+          {" "}
+          <IoCloseCircle />
+        </button>{" "}
+      </div>{" "}
+      {item.completed && <span className="completed"> Done ✅ </span>}{" "}
+    </li>
+  );
 };
 
 export default TodoItem;
